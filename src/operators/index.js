ee41ced2523c898867e15f249a092b1e4144833a
elansuministros-oss <elansuministros@gmail.com>
2026-07-14 08:25:09 -0600

    CRM: exporta operador especializado

--- src/operators/index.js
+++ src/operators/index.js
@@ -2,5 +2,6 @@ export { BaseOperator } from './baseOperator.js';
 export { GeneralOperator } from './generalOperator.js';
 export { SalesOperator } from './salesOperator.js';
 export { TaskOperator } from './taskOperator.js';
+export { CrmOperator, extractSuppliers } from './crmOperator.js';
 export { OperatorRegistry } from './operatorRegistry.js';
 export { createDefaultOperatorRegistry } from './createDefaultOperatorRegistry.js';
